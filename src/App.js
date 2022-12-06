f4f4880e22fe41610e4127b16a568b8031bfcf39
wippew <victor.pawals@gmail.com>
2022-12-06 18:16:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+/* src/App.js */
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import ReactDOM from 'react-dom'
+import Marker from './components/Marker';
 import './App.css';
+import fetchFakeData from './api/fetchFakeData';
+<pre>{process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}</pre>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const App = () => {
+  const mapContainerRef = useRef(null);
+
+  // initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      // See style options here: https://docs.mapbox.com/api/maps/#styles
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [23.252894, 60.875438],
+      zoom: 12.5,
+    });
+
+    // add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+    /* src/App.js */
+    /* src/App.js */
+
+    map.on('load', async () => {
+      // get center coordinates
+      const { lng, lat } = map.getCenter();
+      // fetch new data
+      const results = await fetchFakeData(lng, lat);
+
+      // iterate through the feature collection and append marker to the map for each feature
+      for (let i = 0; i < results.features.length; i++) {
+        const currentRes = results.features[i];
+        const id = i;
+        const vehicle = currentRes.vehicle;
+        const coordinates = currentRes.geometry.coordinates;
+        const order = currentRes.order;
+        if (vehicle == "0") {
+          new mapboxgl.Marker({
+            color: "#FF00FF"
+          })
+            .setLngLat(coordinates)
+            .setPopup(new mapboxgl.Popup({closeOnClick: false, closeButton: false, className:"testboss"}).setText(order))
+            .addTo(map)
+            .togglePopup();
+        } else if(vehicle == "1") {
+          new mapboxgl.Marker({
+            color: "#0000FF"
+          })
+            .setLngLat(coordinates)
+            .setPopup(new mapboxgl.Popup({closeOnClick: false, closeButton: false, className:"testboss"}).setText(order))
+            .addTo(map)
+            .togglePopup();
+        }
+        
+      }
+    });
+
+
+    // clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div className="map-container" ref={mapContainerRef} />;
+};
 
 export default App;
---
+++ src/api/fetchFakeData.js
@@ -0,0 +1,30 @@
+import data from './mockData.json'
+
+
+const fetchFakeData = (lng, lat) => {
+    const newFeaturesList = [];
+    for (let i = 0; i < data.features.length; i++) {
+      const id = i;
+      newFeaturesList[i] = data.features[i];
+    }
+  
+    return Promise.resolve({
+      type: 'FeatureCollection',
+      features: newFeaturesList,
+    });
+  };
+  
+  /**
+   * Generates a random point within 0.025 radius of map center coordinates.
+   * @param {CoordinatePair} centerCoordinates - the {@link CoordinatePair} for the map center
+   * @return {CoordinatePair} randomly generated coordinate pair
+   */
+  const getRandomCoordinate = (lon, lat) => {
+    const r = 0.025 * Math.sqrt(Math.random());
+    const theta = Math.random() * 2 * Math.PI;
+    const latitude = lat + r * Math.cos(theta);
+    const longitude = lon + r * Math.sin(theta);
+    return { longitude, latitude };
+  };
+  
+  export default fetchFakeData;
