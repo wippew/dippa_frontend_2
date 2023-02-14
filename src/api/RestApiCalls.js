598e1217b7305342c0f535a33f075902be0018c8
wippew <victor.pawals@gmail.com>
2023-02-14 13:09:27 +0200

    populating json object now works

--- src/api/RestApiCalls.js
+++ src/api/RestApiCalls.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const testFetch = async (depot1VehicleCount, depot2VehicleCount) => {
     console.log("trying to fetch");
+    const testJson = createMockJsonObject();
     const url = 'http://localhost:8080/getRoutes';
     try {
         const response = await axios.get(url,
@@ -15,6 +16,34 @@ const testFetch = async (depot1VehicleCount, depot2VehicleCount) => {
     }
     return null
 }
+
+const createMockJsonObject = () => {
+    const emptyJsonList = [];
+    createDepot(emptyJsonList);
+    return emptyJsonList;
+}
+
+const createDepot = (emptyJson) => {    
+    const newDepot = {};
+    newDepot.depotName = "depot1";
+    const coordinates = {};
+    coordinates.x = 60.12;
+    coordinates.y = 21.12;
+    const vehicles = [];
+    newDepot.coordinates = coordinates;
+    newDepot.vehicles = vehicles;    
+    addVehicleToDepot("vehicle1", newDepot.vehicles);
+    addVehicleToDepot("vehicle2", newDepot.vehicles);
+    emptyJson.push(newDepot);
+}
+
+const addVehicleToDepot = (vehicleName, garageObject) => {
+    const newVehicle = {};
+    newVehicle.id = vehicleName;
+    newVehicle.competence = 2;
+    garageObject.push(newVehicle);
+}
+
 
 
 export default testFetch;
